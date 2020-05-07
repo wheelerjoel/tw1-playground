5cb9779b7c05b89a9680917fde2e9be47d6f517f
wheelerjoel <63418064+wheelerjoel@users.noreply.github.com>
2020-05-07 14:45:37 -0500

    0.12upgrade
    ran terraform 0.12upgrade command and playing with vlan pools

--- epgs.tf
+++ epgs.tf
@@ -1,28 +1,27 @@
 module "epg1" {
-source = "./epg"
-app_profile_name = "Newco-prod"
-bd_name = "NewCo-prod-bd-1"
-name = "Newco-prod-web"
-vlan_id = "222"
-app_profile_description = "NewCo production Web"
-
+  source                  = "./epg"
+  app_profile_name        = "Newco-prod"
+  bd_name                 = "NewCo-prod-bd-1"
+  name                    = "Newco-prod-web"
+  vlan_id                 = "222"
+  app_profile_description = "NewCo production Web"
 }
-module "epg2" {
-source = "./epg"
-app_profile_name = "Newco-prod"
-bd_name = "NewCo-prod-bd-1"
-name = "NewCo-prod-app"
-vlan_id = "333"
-app_profile_description = "NewCo production App"
 
+module "epg2" {
+  source                  = "./epg"
+  app_profile_name        = "Newco-prod"
+  bd_name                 = "NewCo-prod-bd-1"
+  name                    = "NewCo-prod-app"
+  vlan_id                 = "333"
+  app_profile_description = "NewCo production App"
 }
 
 module "epg3" {
-source = "./epg"
-app_profile_name = "NewCo-UAT"
-bd_name = "NewCo-non-prod-bd"
-name = "NewCo-UAT"
-vlan_id = "1231"
-app_profile_description = "NewCo UAT environment"
-
+  source                  = "./epg"
+  app_profile_name        = "NewCo-UAT"
+  bd_name                 = "NewCo-non-prod-bd"
+  name                    = "NewCo-UAT"
+  vlan_id                 = "1231"
+  app_profile_description = "NewCo UAT environment"
 }
+
---
+++ vlan-pool.tf
@@ -0,0 +1,5 @@
+module "vlan-pool-1" {
+  source         = "./vlan-pool"
+  vlan_pool_name = "NewCo-DR-Vlans"
+}
+
